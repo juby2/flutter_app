1cc88cfdf7cd9124a9a34666a1675fb4fb24e484
Brandy <brinda.sangani@gmail.com>
2022-01-25 11:59:49 +0530

    day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/pages/home_page.dart';
-import 'package:flutter_app/pages/login_page.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -22,11 +20,7 @@ class MyApp extends StatelessWidget {
         // brightness: Brightness.dark,
         primarySwatch: Colors.green
       ),
-      routes:{
-        "/":(context) =>logInPage(),
-        "/login":(context) =>Homepage(),
-        "/login":(context) =>logInPage(),
-      } ,
+      
       );
     
   }
