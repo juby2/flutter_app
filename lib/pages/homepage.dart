be58df2136792436d221bed69e7ded6c3ddf3f16
Brandy <brinda.sangani@gmail.com>
2022-02-03 11:28:45 +0530

    Day 13 completed

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_app/models/catalog.dart';
 import 'package:flutter_app/widgets/drawer.dart';
-import 'package:flutter_app/widgets/item_widgets.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -48,11 +47,31 @@ class _HomepageState extends State<Homepage> {
       body: Padding(
           padding: const EdgeInsets.all(16.0),
           child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-              ? ListView.builder(
-                  itemCount: CatalogModel.items.length,
-                  itemBuilder: (context, index) {
-                    return ItemWidget(item: CatalogModel.items[index]);
-                  })
+              ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16), 
+                itemBuilder: (context,index){
+                  final item = CatalogModel.items[index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                    child: GridTile(
+                      header: Container(
+                        child: Text(item.name,
+                        style: TextStyle(color: Colors.white),),
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple
+                        ),),
+                        
+                      child: Image.network(item.image),
+                      footer: Text(item.price.toString()),
+                      )
+                    );
+                },
+                itemCount: CatalogModel.items.length,
+                )
               : Center(
                   child: CircularProgressIndicator(),
                 )),
